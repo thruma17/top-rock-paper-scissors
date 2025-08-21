67f57651366e7fdaf20b4a6f5ba81c02d92c07c2
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-08-21 19:58:00 +0200

    Add function getHumanChoice

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 // create function named getComputerChoice
-// must return random string between: "rock" "paper" "scissors"
+// must return random string between: 'rock' 'paper' 'scissors'
 let randomNumber = Math.floor(Math.random() * 100) + 1;
-let computerChoice1 = "rock";
-let computerChoice2 = "paper";
-let computerChoice3 = "scissors";
+let choice1 = "rock";
+let choice2 = "paper";
+let choice3 = "scissors";
 
 function getComputerChoice() {
   if (randomNumber >= 1 && randomNumber <= 33) {
-    return computerChoice1;
+    return choice1;
   } else if (randomNumber >= 34 && randomNumber <= 66) {
-    return computerChoice2;
+    return choice2;
   } else if (randomNumber >= 67 && randomNumber <= 99) {
-    return computerChoice3;
+    return choice3;
   }
 }
 
 // console.log(randomNumber);
 
 // console.log(getComputerChoice());
+
+// create function getHumanChoice
+// getHumanChoice() returns rock paper or scissors
+
+let humanChoice = prompt("rock, paper or scissors?", "");
+
+function getHumanChoice() {
+  if (humanChoice === "rock") {
+    return choice1;
+  } else if (humanChoice === "paper") {
+    return choice2;
+  } else if (humanChoice === "scissors") {
+    return choice3;
+  }
+}
+
+console.log(getHumanChoice());
