35f71d7cd4cdd3a7796fc724df845e04a04115e6
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-10-09 19:59:58 +0200

    Create function to handle playerChoice

--- script.js
+++ script.js
@@ -1,94 +1,5 @@
 console.clear();
 
-// creating function to get computer's choice
-
-const computerChoice = getComputerChoice();
-
-function getComputerChoice() {
-  const options = 3;
-  let randomNumber = Math.floor(Math.random() * options + 1);
-  switch (randomNumber) {
-    case 1:
-      return "ROCK";
-    case 2:
-      return "PAPER";
-    case 3:
-      return "SCISSORS";
-  }
-}
-
-// creating functions to get player's choice
-
-// creating variables for choice buttons
-
-const rockBtn = document.getElementById(`rockBtn`);
-const paperBtn = document.getElementById(`paperBtn`);
-const scissorsBtn = document.getElementById(`scissorsBtn`);
-
-// adding event listeners to buttons
-
-rockBtn.addEventListener(`click`, getPlayerChoice);
-paperBtn.addEventListener(`click`, getPlayerChoice);
-scissorsBtn.addEventListener(`click`, getPlayerChoice);
-
-// creating variables to keep track of score
-// creating variables for round result
-
-let playerScore = 0;
-let computerScore = 0;
-let win = `You win, ${playerChoice.toLowerCase()} beats ${computerChoice.toLowerCase()}!`;
-let lose = `You lose, ${computerChoice.toLowerCase()} beats ${playerChoice.toLowerCase()}!`;
-let tie = `Tie! Try again...`;
-
-// writing function to play one round
-// asks player to choose weapon
-// declares the winner
-// adds point to winner
-
-function playRound(playerChoice, computerChoice) {
-  if (playerChoice === computerChoice) {
-    return `${tie}`;
-  }
-  if (
-    (playerChoice === "ROCK" && computerChoice === "SCISSORS") ||
-    (playerChoice === "SCISSORS" && computerChoice === "PAPER") ||
-    (playerChoice === "PAPER" && computerChoice === "ROCK")
-  ) {
-    playerScore++;
-    return `${win}`;
-  }
-  if (
-    (computerChoice === "ROCK" && playerChoice === "SCISSORS") ||
-    (computerChoice === "SCISSORS" && playerChoice === "PAPER") ||
-    (computerChoice === "PAPER" && playerChoice === "ROCK")
-  ) {
-    computerScore++;
-    return `${lose}`;
-  }
-}
-
-// writing function to play a game BO5
-
-let moves = 0;
-
-function playGame() {
-  console.log(playRound());
-  console.log(
-    `Your score: ${playerScore}.\n\n${computerName} score: ${computerScore}\n`
-  );
-  moves++;
-  if (moves < /*5*/ 1) {
-    playGame();
-  } else if (playerScore == computerScore) {
-    // playGame();
-    console.log(`ERROR`);
-  } else if (playerScore > computerScore) {
-    console.log(winnerPlayer);
-  } else {
-    console.log(winnerComputer);
-  }
-}
-
 // writing function to let player choose her/his name and rival's name
 
 let playerName = ``;
@@ -107,12 +18,97 @@ function chooseComputerName() {
 
 // chooseComputerName(); ++++++++++
 
-// winner declaration
+// creating function to get computer's choice
 
-let winnerPlayer = `The winner is...\n\n...\n\n...${playerName.toUpperCase()}`;
+const computerChoice = getComputerChoice();
 
-let winnerComputer = `The winner is...\n\n...\n\n...${computerName.toUpperCase()}`;
+function getComputerChoice() {
+  const options = 3;
+  let randomNumber = Math.floor(Math.random() * options + 1);
+  switch (randomNumber) {
+    case 1:
+      return "ROCK";
+    case 2:
+      return "PAPER";
+    case 3:
+      return "SCISSORS";
+  }
+}
+
+// creating variables for choice buttons
+
+const rockBtn = document.getElementById(`rockBtn`);
+const paperBtn = document.getElementById(`paperBtn`);
+const scissorsBtn = document.getElementById(`scissorsBtn`);
+
+// adding event listeners to buttons
+
+rockBtn.addEventListener(`click`, () => buttonClicked(`ROCK`));
+paperBtn.addEventListener(`click`, () => buttonClicked(`PAPER`));
+scissorsBtn.addEventListener(`click`, () => buttonClicked(`SCISSORS`));
+
+// creating function to get player's choice
+
+function buttonClicked(playerChoice) {
+  playRound(playerChoice, computerChoice);
+  console.log(`Clicked`);
+}
+
+// creating variables to keep track of score
+// creating variables for round result
+
+let playerScore = 0;
+let computerScore = 0;
+
+// writing function to play one round
+// asks player to choose weapon
+// declares the winner
+// adds point to winner
+
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return `Tie! Try again...`;
+  }
+  if (
+    (playerChoice === "ROCK" && computerChoice === "SCISSORS") ||
+    (playerChoice === "SCISSORS" && computerChoice === "PAPER") ||
+    (playerChoice === "PAPER" && computerChoice === "ROCK")
+  ) {
+    playerScore++;
+    return `You win, ${playerChoice.toLowerCase()} beats ${computerChoice.toLowerCase()}!`;
+  }
+  if (
+    (computerChoice === "ROCK" && playerChoice === "SCISSORS") ||
+    (computerChoice === "SCISSORS" && playerChoice === "PAPER") ||
+    (computerChoice === "PAPER" && playerChoice === "ROCK")
+  ) {
+    computerScore++;
+    return `You lose, ${computerChoice.toLowerCase()} beats ${playerChoice.toLowerCase()}!`;
+  }
+}
+
+// writing function to play a game BO5
+
+let moves = 0;
+
+function playGame() {
+  playRound(playerChoice, computerChoice);
+  console.log(
+    `Your score: ${playerScore}.\n\n${computerName} score: ${computerScore}\n`
+  );
+  moves++;
+  if (moves < /*5*/ 1) {
+    playGame();
+  } else if (playerScore == computerScore) {
+    // playGame();
+    console.log(`ERROR`);
+  } else if (playerScore > computerScore) {
+    console.log(`The winner is...\n\n...\n\n...${playerName.toUpperCase()}`);
+  } else {
+    console.log(`The winner is...\n\n...\n\n...${computerName.toUpperCase()}`);
+  }
+}
 
 // command to start the game
 
-playGame();
+// playGame();
